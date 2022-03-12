3da6903a5e8ca2af20f9987c4886433bf521a23e
Gavin Miller <gavinmiller12345@gmail.com>
2022-03-12 09:09:23 +0100

    refactor: Remove unneeded vec & improve variable naming

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,29 @@
 use std::fs::read_to_string;
 
+// advent of code day 1
+// return the number of how many times the depth increases (how many times the number goes up)
+
 fn main() {
-    // advent of code day 1
-    // return the number of how many times the depth increases (how many times the number goes up)
+    let data_raw = read_to_string("./data.txt").unwrap();
+    let data_string_vec: Vec<&str> = data_raw.split("\n").collect(); // this leaves an empty string at the last index
 
-    // array with one index which is a giant string
-    let mut slash_n_vec = Vec::new();
-
-    let data_raw = read_to_string("./data.txt");
-    match &data_raw {
-        Ok(v) => slash_n_vec.push(v), // println!("data is : {}", v),
-        Err(e) => println!("Some Error: {}", e),
-    };
-
-    // array of numbers
-    let string_nums: Vec<&str> = slash_n_vec[0].split("\n").collect();
-    let array_iter = string_nums.iter();
+    println!("data_string_vec: {:?}", data_string_vec);
 
     let mut count: u32 = 0;
     let mut stored: u32 = 0;
 
-    // hold new array of u32s from strings
-    let mut vec_of_nums: Vec<u32> = Vec::new();
+    // hold new vec of u32s from strings
+    let mut data_num_vec: Vec<u32> = Vec::new();
 
     // convert strings into nums
-    for val in array_iter {
+    for val in data_string_vec.iter() {
         if val.len() != 0 {
             let parsed = val.parse::<u32>().unwrap();
-            //  println!("current i: {i}");
-            vec_of_nums.push(parsed);
+            data_num_vec.push(parsed);
         }
     }
 
-    let iter_nums = vec_of_nums.iter();
-
-    for (i, val) in iter_nums.enumerate() {
+    for (i, val) in data_num_vec.iter().enumerate() {
         // val at first index just gets stored
         if i == 0 {
             stored = val.clone();
