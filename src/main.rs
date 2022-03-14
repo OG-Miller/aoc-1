31acf47337a73611ae57b86c290abc7cf4e150e1
Gavin Miller <gavinmiller12345@gmail.com>
2022-03-14 21:31:04 +0100

    Day2A solved

--- src/main.rs
+++ src/main.rs
@@ -4,45 +4,52 @@ use std::fs::read_to_string;
 // calculate the final horizontal & depth positions, return them multiplied together
 
 fn main() {
-    struct Position {
-        depth: u8,
-        horizontal: u8,
-    }
-    // overall position to be updated
-    let mut store_position = Position {
-        depth: 0,
-        horizontal: 0,
-    };
+    // [0] = depth [1] = horizontal
+    let mut tuple_store: (u32, u32) = (0, 0);
+
     #[derive(Debug)]
     struct Command {
         direction: String,
-        val: u8,
+        val: u32,
     }
 
     let data_string = read_to_string("./data.txt").unwrap();
     let data_string_split: Vec<&str> = data_string.split("\n").collect();
-    //println!("{:?}", data_string_split); // eg  ["forward 8", "down 6", "down 6"]
 
-    let command_vec_result: Vec<Command> = string_vec_to_command_vec(data_string_split);
-    println!("command_vec_result: {:?}", command_vec_result);
+    let command_vec_result: Vec<Command> = make_command_vec(data_string_split);
 
-    fn string_vec_to_command_vec(string_vec: Vec<&str>) -> Vec<Command> {
+    fn make_command_vec(string_vec: Vec<&str>) -> Vec<Command> {
         let mut inner_command_vec: Vec<Command> = vec![];
 
         string_vec.iter().for_each(|s| {
             let dir_and_val_string_vec: Vec<&str> = s.split(" ").collect();
+
             // build current_command
             let current_command = Command {
                 direction: String::from(dir_and_val_string_vec[0]),
                 val: if s.len() > 0 {
-                    dir_and_val_string_vec[1].parse::<u8>().unwrap()
+                    dir_and_val_string_vec[1].parse::<u32>().unwrap()
                 } else {
                     0
                 },
             };
             inner_command_vec.push(current_command);
         });
-        // return vec of commands
         inner_command_vec
     }
+
+    command_vec_result.iter().for_each(|com| {
+        // [0] = depth [1] = horizontal
+        if com.direction == "forward" {
+            tuple_store.1 += com.val;
+        } else if com.direction == "down" {
+            tuple_store.0 += com.val;
+        } else if com.direction == "up" {
+            tuple_store.0 -= com.val;
+        } else {
+            println!(" in else block, case not covered");
+        }
+    });
+
+    println!("tuple_store multiplied{:?}", tuple_store.0 * tuple_store.1);
 }
