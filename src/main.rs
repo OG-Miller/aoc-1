094f26fc20d172d79a945158c4dc3a0aa9288509
Gavin Miller <gavinmiller12345@gmail.com>
2022-03-21 18:12:38 +0100

    refactor: move fn one out of main

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,6 @@ use std::fs::read_to_string;
 fn main() {
     // AOC Day3A
     let data_string = read_to_string("./data.txt").unwrap();
-
-    fn one(s: String) -> Vec<u64> {
-        let mut commons_vec: Vec<u64> = vec![0; 12];
-        let mut index: usize = 0;
-        let mut num_of_zeros: u64 = 0;
-
-        while index < 12 {
-            s.lines().for_each(|l| {
-                if l.chars().nth(index).unwrap_or('x') == '0' {
-                    num_of_zeros += 1;
-                }
-                commons_vec[index] = num_of_zeros;
-            });
-
-            num_of_zeros = 0;
-            index += 1;
-        }
-
-        commons_vec
-    }
-
     let commons_result = one(data_string);
     let gamma: String = commons_result
         .into_iter()
@@ -40,4 +19,24 @@ fn main() {
     let solution = decimal_epsilon.unwrap() * decimal_gamma.unwrap();
 
     println!("solution: {:?}", solution);
+}
+
+pub fn one(s: String) -> Vec<u64> {
+    let mut commons_vec: Vec<u64> = vec![0; 12];
+    let mut index: usize = 0;
+    let mut num_of_zeros: u64 = 0;
+
+    while index < 12 {
+        s.lines().for_each(|l| {
+            if l.chars().nth(index).unwrap_or('x') == '0' {
+                num_of_zeros += 1;
+            }
+            commons_vec[index] = num_of_zeros;
+        });
+
+        num_of_zeros = 0;
+        index += 1;
+    }
+
+    commons_vec
 }
