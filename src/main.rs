b039150bdad937fb193d514c97d188af6e2af629
Gavin Miller <gavinmiller12345@gmail.com>
2022-03-21 14:19:13 +0100

    Day3A solved

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,43 @@
 use std::fs::read_to_string;
 
 fn main() {
+    // AOC Day3A
     let data_string = read_to_string("./data.txt").unwrap();
 
-    // this function should return most common digit for index on each line
     fn one(s: String) -> Vec<u64> {
         let mut commons_vec: Vec<u64> = vec![0; 12];
         let mut index: usize = 0;
         let mut num_of_zeros: u64 = 0;
 
-        while index <= 13 {
+        while index < 12 {
             s.lines().for_each(|l| {
-                if l.chars().nth(index).unwrap() == '0' {
-                    //  println!("char == 0");
+                if l.chars().nth(index).unwrap_or('x') == '0' {
                     num_of_zeros += 1;
-                } else {
-                    println!("char is not 0");
                 }
                 commons_vec[index] = num_of_zeros;
             });
 
             num_of_zeros = 0;
             index += 1;
-            println!("commons_vec: {:?}", commons_vec);
         }
 
         commons_vec
     }
 
     let commons_result = one(data_string);
+    let gamma: String = commons_result
+        .into_iter()
+        .map(|x| if x > 500 { "0" } else { "1" })
+        .collect();
 
-    // finding gamma:
-    //let mut gamma: String = String::new();
-    let gamma = commons_result.iter().map(|n| n.to_string());
-    println!("{:?}", gamma);
-    //commons_result.iter().for_each(|n| gamma.push(n.to_char()));
+    let epsilon: String = gamma
+        .chars()
+        .map(|c| if c == '1' { "0" } else { "1" })
+        .collect();
+
+    let decimal_gamma = isize::from_str_radix(&gamma, 2);
+    let decimal_epsilon = isize::from_str_radix(&epsilon, 2);
+    let solution = decimal_epsilon.unwrap() * decimal_gamma.unwrap();
+
+    println!("solution: {:?}", solution);
 }
