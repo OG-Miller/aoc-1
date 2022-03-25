d95d5dd8b3705ab98f8ca508d359e244ccdc0727
Gavin Miller <gavinmiller12345@gmail.com>
2022-03-25 13:13:45 +0100

    Day3B solved

--- src/main.rs
+++ src/main.rs
@@ -46,44 +46,105 @@ fn main() {
 
     //println!("solution: {:?}", solution);
 
-    let ox = func(data_string.lines().collect::<Vec<&str>>(), gamma, &mut 0);
+    let ox = func(
+        data_string.lines().collect::<Vec<&str>>(),
+        gamma,
+        true,
+        &mut 0,
+    );
     println!("ox: {:?}", ox);
 
-    let co2 = func(data_string.lines().collect::<Vec<&str>>(), epsilon, &mut 0);
+    let co2 = func(
+        data_string.lines().collect::<Vec<&str>>(),
+        epsilon,
+        false,
+        &mut 0,
+    );
     println!("co2: {:?}", co2);
 
-    let decimal_ox = isize::from_str_radix(&ox[0], 2).unwrap();
-    println!("ox[0]: {} , decimal_ox: {:?}", ox[0], decimal_ox);
-    let decimal_co2 = isize::from_str_radix(&co2[0], 2).unwrap();
-    println!("decimal_co2: {:?}", decimal_co2);
+    let decimal_ox = isize::from_str_radix(&ox[0], 2).unwrap_or(7);
+    println!("ox[0]: {}, decimal_ox: {:?}", ox[0], decimal_ox);
+    let decimal_co2 = isize::from_str_radix(&co2[0], 2).unwrap_or(8);
+    println!("co2[0]: {}, decimal_co2: {:?}", co2[0], decimal_co2);
 
     println!("{:?}", decimal_ox * decimal_co2);
 }
 
 // ------------
-
-pub fn func<'a>(v: Vec<&'a str>, gamma: String, i: &mut u8) -> Vec<&'a str> {
+// you have to re-calculate the most common column after every filter loop
+pub fn func<'a>(
+    v: Vec<&'a str>,
+    gamma_or_epsilon: String,
+    gamma_bool: bool,
+    i: &mut u8,
+) -> Vec<&'a str> {
     let filtered = v
         .into_iter()
         .filter(|s| {
             let a = s.chars().nth(*i as usize).unwrap_or('x');
-            let b = gamma.chars().nth(*i as usize).unwrap();
+            let b = gamma_or_epsilon.chars().nth(*i as usize).unwrap();
             //println!("if a:{} = b:{}, keep line : {}", a, b, s);
             //       println!("index:{i}");
             a == b
         })
         .collect::<Vec<&str>>();
-    println!("{:?}", filtered);
+    // println!("{:?}", filtered);
     *i += 1;
-    println!("index{:?}", i);
-    if i < &mut 12 && filtered.len() > 0 {
-        func(filtered, gamma, i)
+    //println!("index{:?} , filtered len:{}", i, filtered.len());
+    if i < &mut 12 && filtered.len() > 1 {
+        let local_line_count_div_2 = filtered.len() / 2 as usize;
+        let new_zeros = inner_zeros_per_column(&filtered);
+
+        let new_gamma: String = new_zeros
+            .into_iter()
+            // if equally common keep the 1
+            .map(|x| {
+                if x as usize > local_line_count_div_2 {
+                    if gamma_bool == true {
+                        "0"
+                    } else {
+                        "1"
+                    }
+                } else {
+                    if gamma_bool == true {
+                        "1"
+                    } else {
+                        "0"
+                    }
+                }
+            })
+            .collect();
+
+        // now do this for epsilon
+
+        func(filtered, new_gamma, gamma_bool, i)
     } else {
         filtered
     }
 }
 
-// ------------
+//-----
+pub fn inner_zeros_per_column(v: &Vec<&str>) -> Vec<u64> {
+    let mut commons_vec: Vec<u64> = vec![0; 12];
+    let mut index: usize = 0;
+    let mut num_of_zeros: u64 = 0;
+
+    while index < 12 {
+        v.iter().for_each(|x| {
+            if x.chars().nth(index).unwrap() == '0' {
+                num_of_zeros += 1;
+            }
+            commons_vec[index] = num_of_zeros;
+        });
+
+        num_of_zeros = 0;
+        index += 1;
+    }
+
+    commons_vec
+}
+
+// ----------------------------------------------------------------------------------
 
 pub fn zeros_per_column(s: &String) -> Vec<u64> {
     let mut commons_vec: Vec<u64> = vec![0; 12];
@@ -92,7 +153,7 @@ pub fn zeros_per_column(s: &String) -> Vec<u64> {
 
     while index < 12 {
         s.lines().for_each(|l| {
-            if l.chars().nth(index).unwrap_or('x') == '0' {
+            if l.chars().nth(index).unwrap() == '0' {
                 num_of_zeros += 1;
             }
             commons_vec[index] = num_of_zeros;
