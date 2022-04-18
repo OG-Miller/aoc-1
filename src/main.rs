dd46b765b382d8e6bc4d32bad752e468b0856dd1
Gavin Miller <gavinmiller12345@gmail.com>
2022-04-18 21:42:48 +0200

    wip: Currently returning winning boards but accompanying data is broken

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,15 @@ use std::fs::read_to_string; // AOC Day4A
 fn main() {
     //-------------  Build backlog of numbers to call -------------------------------
 
-    let data_string = read_to_string("./4a-numbers.txt").unwrap();
+    let data_string = read_to_string("./test-bingo-numbers.txt").unwrap();
 
     let backlog_nums = data_string
         .split(",")
         .filter(|x| *x != "\n")
         .map(|n| n.parse::<u8>().unwrap())
         .collect::<Vec<u8>>();
-    //println!("backlog_nums: {:?}", backlog_nums);
     //----------------- Build one vec of boards nums ---------------------------------
-    let boards_string = read_to_string("./4a-bingo-boards.txt").unwrap();
+    let boards_string = read_to_string("./test-bingo-boards.txt").unwrap();
     let alternative = boards_string.replace("\n", ",");
     let replace_spaces = alternative.replace(",", " ");
     let split_stuff = replace_spaces.split(" ").collect::<Vec<&str>>();
@@ -20,74 +19,202 @@ fn main() {
         .into_iter()
         .filter(|x| x.len() > 0)
         .collect::<Vec<&str>>();
-    let all_board_bool = remove_empties
+    let all_board_bool: Vec<(u8, bool)> = remove_empties
         .iter()
         .map(|x| (x.parse::<u8>().unwrap(), false))
-        .collect::<Vec<(u8, bool)>>();
+        .collect();
 
-    let all_boards_vec: Vec<Vec<(u8, bool)>> = build_vec_of_boards(all_board_bool, Vec::new());
-    //println!("all_boards_vec: {:?}", all_boards_vec);
-
-    //--- this will have to be recursive and take the returned vec from each backlog_num search and
-    //use that in the next backlog_num search
-
-    // let find_all_backlog_nums = backlog_nums
+    //--------------------------------------------------
+    //(vec_of_invidual_boards: Vec<Vec<(u8, bool)>>, backlog_nums: Vec<u8>) {
+    //let ret_vec: Vec<Vec<(u8, bool)>> = Vec::new();
+    //let vec_of_boards = build_vec_of_boards(&all_board_bool, Vec::new());
+    //    vec_of_boards
+    //        .iter()
+    //        .for_each(|board| check_for_winner(board.to_vec(), backlog_nums));
+    let mut v = Vec::new();
+    // let x = vec_of_boards
     //     .iter()
-    //     .for_each(|&n| find_backlog_in_all_boards(&all_boards_vec, n));
-    // //.collect();
-    // println!("fin: {:?}", find_all_backlog_nums);
+    //     .for_each(|board| v.push(check_for_winner(board.to_vec(), backlog_nums)));
+    let winners = build_vec_of_boards(&all_board_bool, Vec::new())
+        .iter()
+        .for_each(|board| v.push(check_for_winner(board.to_vec(), &backlog_nums)));
+
+    println!("vvvvvvvvvvvvvvvvvvvvvv: {:?}", v);
+}
+#[derive(Debug)]
+pub struct WinnerCandidate {
+    board: Vec<(u8, bool)>, // to use in final calculation
+    winning_number: u8,     // to use in final calculation
+    called_num_count: u8,   // so we know what order boards win in.
 }
 
-fn tester2(boards: Vec<Vec<(u8, bool)>>, backlog: Vec<u8>) {
-    let mut inner_vec = Vec::new();
-    inner_vec.push(backlog.split_at(1));
+pub fn check_for_winner(one_board: Vec<(u8, bool)>, backlog: &Vec<u8>) -> Option<WinnerCandidate> {
+    // board for struct
+    let mut candidate_board: Vec<(u8, bool)> = Vec::new();
+    // winning_number for struct
+    let mut current_backlog_num: u8 = 0; // this is returning last number in list (2) but it should actually be 4
+    let mut called_num_count: u8 = 0; // this is currently returning 25, this should be how many to get to a win...
+    let mut is_a_winner: bool = false;
+    //let mut winning_board_index: usize = 26;
+
+    //  let w = WinnerCandidate {
+    //      board: candidate_board,
+    //      winning_number: current_backlog_num,
+    //      called_num_count,
+    //  };
+    //candidate_board = one_board;
     backlog
         .iter()
-        .for_each(|&n| find_backlog_in_all_boards(&all_boards_vec, n))
+        .enumerate()
+        .for_each(|(_backlog_i, backlog_n)| {
+            called_num_count += 1;
+            one_board
+                .iter()
+                .enumerate()
+                .for_each(|(board_i, board_value)| {
+                    println!("board_value:{:?}, backlog n: {:?}", board_value, backlog_n);
+                    current_backlog_num = *backlog_n; //.clone();
+                    println!("current_backlog_num: {:?}", current_backlog_num);
+                    // populate canditate board
+                    if candidate_board.len() < 25 {
+                        if backlog_n == &board_value.0 {
+                            //println!(" MATCH!");
+                            //println!("one_board: {:?}", &one_board);
+                            candidate_board.push((board_value.0, true));
+                            // check if board is winner----------------------------------<<<<<<
+                            check_current_iter_for_winner(&candidate_board);
+                        } else {
+                            // no match
+                            candidate_board.push((board_value.0, false));
+                        }
+                    } else {
+                        //println!("candidate_board.len() is >= 25");
+                        if backlog_n == &board_value.0 {
+                            candidate_board[board_i] = (board_value.0, true);
+                            // check if board is winner----------------------------------<<<<<<
+                            check_current_iter_for_winner(&candidate_board);
+                        }
+                    }
+                });
+            //-------------- logic to check columns
+            //       candidate_board.iter().enumerate().for_each(|(i, num)| {
+            //           if (i < 5 )//&& is_a_winner == false)
+            //               && (num.1 == true)
+            //               && (candidate_board[i + 5].1 == true)
+            //               && (candidate_board[i + 10].1 == true)
+            //               && (candidate_board[i + 15].1 == true)
+            //               && (candidate_board[i + 20].1 == true)
+            //           {
+            //               //println!("column bingo"); // you can return after the first bingo, currently still checks after first
+            //               is_a_winner = true;
+            //           }
+            //           //else {
+            //           //println!("no bingo");
+            //           //is_a_winner = false;
+            //           //   return;
+            //           //}
+            //       });
+
+            //       //----------------- logic to check rows
+            //       candidate_board.iter().enumerate().for_each(|(i, num)| {
+            //           if (i == 0 | 5 | 10 | 15 | 20)
+            //               && (num.1 == true)
+            //               && (candidate_board[i + 5].1 == true)
+            //               && (candidate_board[i + 10].1 == true)
+            //               && (candidate_board[i + 15].1 == true)
+            //               && (candidate_board[i + 20].1 == true)
+            //           {
+            //               //println!("row bingo");
+            //               //println!("candidate_board: {:?}", candidate_board);
+            //               is_a_winner = true;
+            //               // println!("is_a_winner:{:?}", &is_a_winner);
+            //           } else {
+            //               //        println!("no bingo");
+            //               //is_a_winner = false;
+            //               return;
+            //           }
+            //       })
+        });
+    // if board is a winning canditate return it, else...don't?
+    //println!("candidate_board:{:?}", candidate_board);
+    let w = WinnerCandidate {
+        board: candidate_board,
+        winning_number: current_backlog_num,
+        called_num_count,
+    };
+    //if is_a_winner == true {
+    Some(w)
+    //Some(w)
+    //} else {
+    //    println!("is_a_winner:{:?}", &is_a_winner);
+    //None
+    //}
 }
 
-//search all boards for one called number(to test for now)then iterate all backlog with this fn
-// This has to be recursive because each checked number bool has to be updated and passed into the
-// same fn again....
-//
-pub fn find_backlog_in_all_boards(
-    boards: &Vec<Vec<(u8, bool)>>,
-    called: u8,
-) -> Vec<Vec<(u8, bool)>> {
-    let mut all_searched_boards: Vec<Vec<(u8, bool)>> = Vec::new();
-    boards
-        .iter()
-        .for_each(|board| all_searched_boards.push(search_single_board(board, called)));
-    all_searched_boards
-}
-//let tester = search_single_board(&all_boards_vec[0], backlog_nums[0]);
-//println!("tester: {:?}", tester);
-//-- For each backlog_num, find matches and update bool
+pub fn check_current_iter_for_winner(cand_board: &Vec<(u8, bool)>) -> bool {
+    if cand_board.len() < 25 {
+        false
+    } else {
+        let mut is_a_winner: bool = false;
+        //----------------- logic to check column
+        cand_board.iter().enumerate().for_each(|(i, num)| {
+            if (i < 5)
+                && (num.1 == true)
+                && (cand_board[i + 5].1 == true)
+                && (cand_board[i + 10].1 == true)
+                && (cand_board[i + 15].1 == true)
+                && (cand_board[i + 20].1 == true)
+            {
+                //println!("column bingo"); // you can return after the first bingo, currently still checks after first
+                is_a_winner = true;
+            } //else {
+              //println!("no bingo");
+              //is_a_winner = false;
+              //   return;
+              //}
+        });
 
-pub fn search_single_board(board: &Vec<(u8, bool)>, called: u8) -> Vec<(u8, bool)> {
-    let mut searched_board = Vec::new();
-    board.iter().for_each(|&t| {
-        if t.0 == called {
-            searched_board.push((t.0, true))
+        //----------------- logic to check rows
+        cand_board.iter().enumerate().for_each(|(i, num)| {
+            if (i == 0 | 5 | 10 | 15 | 20)
+                && (num.1 == true)
+                && (cand_board[i + 5].1 == true)
+                && (cand_board[i + 10].1 == true)
+                && (cand_board[i + 15].1 == true)
+                && (cand_board[i + 20].1 == true)
+            {
+                //println!("row bingo");
+                //println!("candidate_board: {:?}", candidate_board);
+                is_a_winner = true;
+                // println!("is_a_winner:{:?}", &is_a_winner);
+                //} else {
+                //        println!("no bingo");
+                //is_a_winner = false;
+                //return;
+                //   false;
+            }
+        });
+        is_a_winner
+    }
+}
+
+// here I want to return maybe the index and the value of each match, maybe the whole vec
+pub fn all_indexes_of_one_backlog(boards: &Vec<(u8, bool)>, num: u8) -> Vec<(u8, bool)> {
+    let mut inner_vec = Vec::new();
+    boards.iter().enumerate().for_each(|(_i, &x)| {
+        if x.0 == num {
+            inner_vec.push((x.0, true))
         } else {
-            searched_board.push(t)
+            inner_vec.push(x)
         }
     });
-    searched_board
+    inner_vec
 }
-//---------- Build vec of board number tuples with 'picked' bool (u8,bool) -----------
-
-//let tuple_vec = all_boards_tuple(all_board_nums);
-//    let check_all_boards = backlog_nums
-//        .iter()
-//        .map(|&n| check_backlog_against_boards(&tuple_vec, n))
-//        .collect::<Vec<(u8, bool)>>();
-//    println!("check_all_boards: {:?}", check_all_boards);
 
 //---------- Build vec of boards -------------------------------------
 
 fn build_vec_of_boards(
-    all_boards: Vec<(u8, bool)>,
+    all_boards: &Vec<(u8, bool)>,
     ret_vec: Vec<Vec<(u8, bool)>>,
 ) -> Vec<Vec<(u8, bool)>> {
     let mut return_vec: Vec<Vec<(u8, bool)>> = ret_vec;
@@ -95,7 +222,7 @@ fn build_vec_of_boards(
     return_vec.push(split.0.to_vec());
     let remaining_boards = split.1.to_vec();
     if remaining_boards.len() > 25 {
-        build_vec_of_boards(remaining_boards, return_vec)
+        build_vec_of_boards(&remaining_boards, return_vec)
     } else {
         return_vec
     }
@@ -110,18 +237,3 @@ pub fn all_boards_tuple(boards: Vec<u8>) -> Vec<(u8, bool)> {
         .for_each(|n| checked_tuple_vec.push((*n, false)));
     checked_tuple_vec
 }
-
-//------ for each backlog_nums------------------------------------
-
-//pub fn check_backlog_against_boards(tuple_boards: &Vec<(u8, bool)>, backlog_num: u8) -> (u8, bool) {
-//    //let mut checked: Vec<(u8, bool)> = Vec::new();
-//    let mut tup: (u8, bool) = (0, false);
-//    tuple_boards.iter().for_each(|&t| {
-//        if backlog_num == t.0 {
-//            tup = (t.0, true);
-//        } else {
-//            tup = t;
-//        }
-//    });
-//    tup
-//}
