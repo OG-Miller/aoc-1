1e87e4c480e58dc7653a1b09815dba835706a03b
Gavin Miller <gavinmiller12345@gmail.com>
2022-04-23 14:07:32 +0200

    feat: Row check is working

--- src/main.rs
+++ src/main.rs
@@ -25,190 +25,147 @@ fn main() {
         .collect();
 
     //--------------------------------------------------
-    //(vec_of_invidual_boards: Vec<Vec<(u8, bool)>>, backlog_nums: Vec<u8>) {
-    //let ret_vec: Vec<Vec<(u8, bool)>> = Vec::new();
-    //let vec_of_boards = build_vec_of_boards(&all_board_bool, Vec::new());
-    //    vec_of_boards
-    //        .iter()
-    //        .for_each(|board| check_for_winner(board.to_vec(), backlog_nums));
-    let mut v = Vec::new();
-    // let x = vec_of_boards
-    //     .iter()
-    //     .for_each(|board| v.push(check_for_winner(board.to_vec(), backlog_nums)));
-    let winners = build_vec_of_boards(&all_board_bool, Vec::new())
-        .iter()
-        .for_each(|board| v.push(check_for_winner(board.to_vec(), &backlog_nums)));
+    let mut list_of_winners = Vec::new();
 
-    println!("vvvvvvvvvvvvvvvvvvvvvv: {:?}", v);
+    build_vec_of_boards(&all_board_bool, Vec::new())
+        .iter()
+        .for_each(|board| {
+            list_of_winners.push(check_board_for_winner(board.to_vec(), &backlog_nums))
+        });
+
+    println!("list_of_winners: {:?}", list_of_winners);
 }
 #[derive(Debug)]
 pub struct WinnerCandidate {
-    board: Vec<(u8, bool)>, // to use in final calculation
-    winning_number: u8,     // to use in final calculation
-    called_num_count: u8,   // so we know what order boards win in.
+    board: Vec<(u8, bool)>,    // to use in final calculation
+    winning_number: u8,        // to use in final calculation
+    winning_backlog_index: u8, // so we know what order boards win in.
 }
 
-pub fn check_for_winner(one_board: Vec<(u8, bool)>, backlog: &Vec<u8>) -> Option<WinnerCandidate> {
-    // board for struct
-    let mut candidate_board: Vec<(u8, bool)> = Vec::new();
-    // winning_number for struct
-    let mut current_backlog_num: u8 = 0; // this is returning last number in list (2) but it should actually be 4
-    let mut called_num_count: u8 = 0; // this is currently returning 25, this should be how many to get to a win...
-    let mut is_a_winner: bool = false;
-    //let mut winning_board_index: usize = 26;
+pub fn check_board_for_winner(
+    one_board: Vec<(u8, bool)>,
+    backlog: &Vec<u8>,
+) -> Option<WinnerCandidate> {
+    let mut candidate_board: Vec<(u8, bool)> = vec![(0, false); 25];
+    //let mut current_backlog_num: u8 = 0;
+    let mut winner_backlog_i: usize = 0;
+    let mut winner_backlog_n: u8 = 0;
+    let mut found_a_winner: bool = false;
+    let mut row_win: bool = false;
+    let mut winning_number = 0;
+    let mut winning_index: u8 = 0;
+    let mut winning_board_index: usize = 0;
+    let mut winning_board_index_vec: Vec<u8> = Vec::new();
+    let mut winning_backlog_num_vec: Vec<u8> = Vec::new();
 
-    //  let w = WinnerCandidate {
-    //      board: candidate_board,
-    //      winning_number: current_backlog_num,
-    //      called_num_count,
-    //  };
-    //candidate_board = one_board;
     backlog
         .iter()
         .enumerate()
-        .for_each(|(_backlog_i, backlog_n)| {
-            called_num_count += 1;
+        .for_each(|(backlog_i, backlog_n)| {
             one_board
                 .iter()
                 .enumerate()
                 .for_each(|(board_i, board_value)| {
-                    println!("board_value:{:?}, backlog n: {:?}", board_value, backlog_n);
-                    current_backlog_num = *backlog_n; //.clone();
-                    println!("current_backlog_num: {:?}", current_backlog_num);
-                    // populate canditate board
-                    if candidate_board.len() < 25 {
-                        if backlog_n == &board_value.0 {
-                            //println!(" MATCH!");
-                            //println!("one_board: {:?}", &one_board);
-                            candidate_board.push((board_value.0, true));
-                            // check if board is winner----------------------------------<<<<<<
-                            check_current_iter_for_winner(&candidate_board);
-                        } else {
-                            // no match
-                            candidate_board.push((board_value.0, false));
+                    // EVERYTHING IN HERE IS FOR ONE BOARD VALUE----
+                    if &board_value.0 == backlog_n {
+                        // WE HAVE A MATCH..NOW MARK IT ON THE BOARD
+                        candidate_board[board_i] = (board_value.0, true);
+                        let check = check_current_iter_for_winner(&candidate_board);
+                        // check.0 true is a win, check.1 true is a row win
+                        if check.0 == true {
+                            // start of winning block ----------------
+
+                            row_win = check.1;
+
+                            winning_number = if row_win == true {
+                                winner_backlog_n
+                            } else if row_win == false {
+                                winner_backlog_n
+                            } else {
+                                100 // debugging
+                            };
+
+                            winning_board_index_vec.push(backlog_i as u8);
+                            winning_backlog_num_vec.push(*backlog_n);
+                            // if found_a_winner == false {
+                            found_a_winner = true;
+                            winner_backlog_i = backlog_i;
+                            println!("first winner_backlog_i: {backlog_i}");
+                            winner_backlog_n = *backlog_n;
+                            println!("first winner_backlog_n: {backlog_n}");
+
+                            //------------------------------------------------------------- end of winning case block
                         }
-                    } else {
-                        //println!("candidate_board.len() is >= 25");
-                        if backlog_n == &board_value.0 {
-                            candidate_board[board_i] = (board_value.0, true);
-                            // check if board is winner----------------------------------<<<<<<
-                            check_current_iter_for_winner(&candidate_board);
-                        }
+
+                        //winning_number = winner
+
+                        winning_index = if row_win { *backlog_n } else { *backlog_n };
                     }
+                    // -----------------^ FOR EACH BOARD NUMBER ----
                 });
-            //-------------- logic to check columns
-            //       candidate_board.iter().enumerate().for_each(|(i, num)| {
-            //           if (i < 5 )//&& is_a_winner == false)
-            //               && (num.1 == true)
-            //               && (candidate_board[i + 5].1 == true)
-            //               && (candidate_board[i + 10].1 == true)
-            //               && (candidate_board[i + 15].1 == true)
-            //               && (candidate_board[i + 20].1 == true)
-            //           {
-            //               //println!("column bingo"); // you can return after the first bingo, currently still checks after first
-            //               is_a_winner = true;
-            //           }
-            //           //else {
-            //           //println!("no bingo");
-            //           //is_a_winner = false;
-            //           //   return;
-            //           //}
-            //       });
-
-            //       //----------------- logic to check rows
-            //       candidate_board.iter().enumerate().for_each(|(i, num)| {
-            //           if (i == 0 | 5 | 10 | 15 | 20)
-            //               && (num.1 == true)
-            //               && (candidate_board[i + 5].1 == true)
-            //               && (candidate_board[i + 10].1 == true)
-            //               && (candidate_board[i + 15].1 == true)
-            //               && (candidate_board[i + 20].1 == true)
-            //           {
-            //               //println!("row bingo");
-            //               //println!("candidate_board: {:?}", candidate_board);
-            //               is_a_winner = true;
-            //               // println!("is_a_winner:{:?}", &is_a_winner);
-            //           } else {
-            //               //        println!("no bingo");
-            //               //is_a_winner = false;
-            //               return;
-            //           }
-            //       })
         });
-    // if board is a winning canditate return it, else...don't?
-    //println!("candidate_board:{:?}", candidate_board);
-    let w = WinnerCandidate {
-        board: candidate_board,
-        winning_number: current_backlog_num,
-        called_num_count,
-    };
-    //if is_a_winner == true {
-    Some(w)
-    //Some(w)
-    //} else {
-    //    println!("is_a_winner:{:?}", &is_a_winner);
-    //None
-    //}
-}
-
-pub fn check_current_iter_for_winner(cand_board: &Vec<(u8, bool)>) -> bool {
-    if cand_board.len() < 25 {
-        false
+    if winning_backlog_num_vec.len() > 0 {
+        let w = WinnerCandidate {
+            board: candidate_board,
+            winning_number: winning_backlog_num_vec[0],
+            winning_backlog_index: winning_board_index_vec[0],
+        };
+        return Some(w);
     } else {
-        let mut is_a_winner: bool = false;
-        //----------------- logic to check column
-        cand_board.iter().enumerate().for_each(|(i, num)| {
-            if (i < 5)
-                && (num.1 == true)
-                && (cand_board[i + 5].1 == true)
-                && (cand_board[i + 10].1 == true)
-                && (cand_board[i + 15].1 == true)
-                && (cand_board[i + 20].1 == true)
-            {
-                //println!("column bingo"); // you can return after the first bingo, currently still checks after first
-                is_a_winner = true;
-            } //else {
-              //println!("no bingo");
-              //is_a_winner = false;
-              //   return;
-              //}
-        });
-
-        //----------------- logic to check rows
-        cand_board.iter().enumerate().for_each(|(i, num)| {
-            if (i == 0 | 5 | 10 | 15 | 20)
-                && (num.1 == true)
-                && (cand_board[i + 5].1 == true)
-                && (cand_board[i + 10].1 == true)
-                && (cand_board[i + 15].1 == true)
-                && (cand_board[i + 20].1 == true)
-            {
-                //println!("row bingo");
-                //println!("candidate_board: {:?}", candidate_board);
-                is_a_winner = true;
-                // println!("is_a_winner:{:?}", &is_a_winner);
-                //} else {
-                //        println!("no bingo");
-                //is_a_winner = false;
-                //return;
-                //   false;
-            }
-        });
-        is_a_winner
+        return None;
     }
 }
 
-// here I want to return maybe the index and the value of each match, maybe the whole vec
-pub fn all_indexes_of_one_backlog(boards: &Vec<(u8, bool)>, num: u8) -> Vec<(u8, bool)> {
-    let mut inner_vec = Vec::new();
-    boards.iter().enumerate().for_each(|(_i, &x)| {
-        if x.0 == num {
-            inner_vec.push((x.0, true))
-        } else {
-            inner_vec.push(x)
+pub fn check_current_iter_for_winner(board: &Vec<(u8, bool)>) -> (bool, bool) {
+    let mut is_true: u8 = 0;
+    let mut row_win = false;
+    // let mut column_win_count = 0;
+    // let mut row_win_count = 0;
+
+    println!("board.len() {:?}", board.len());
+    //----------------- logic to check column
+    board.iter().enumerate().for_each(|(i, num)| {
+        println!("{i}");
+        if (i < 5 && is_true == 0)
+            && (num.1 == true)
+            && (board[i + 5].1 == true)
+            && (board[i + 9].1 == true)
+            && (board[i + 14].1 == true)
+            && (board[i + 19].1 == true)
+        {
+            //   println!("i is ---> {i}");
+            println!(" num.0 {:?}: ", num.0);
+            println!(" board[i + 5].0 {:?}: ", board[i + 5].0);
+            println!(" board[i + 10].0 {:?}: ", board[i + 10].0);
+            println!(" board[i + 15].0 {:?}: ", board[i + 15].0);
+            println!(" board[i + 20].0 {:?}: ", board[i + 20].0);
+            is_true += 1;
+            //        column_win_count += 1;
         }
     });
-    inner_vec
+
+    //----------------- logic to check rows
+    let row_vec = vec![0, 5, 10, 15, 20];
+    board.iter().enumerate().for_each(|(i, num)| {
+        //println!("trying to check row... ");
+        if row_vec.contains(&i)
+            && (num.1 == true)
+            && (board[i + 1].1 == true)
+            && (board[i + 2].1 == true)
+            && (board[i + 3].1 == true)
+            && (board[i + 4].1 == true)
+        {
+            //       row_win_count += 1;
+            is_true += 1;
+            row_win = true;
+        }
+    });
+    // return (win?: bool, row win: bool)
+    if is_true > 0 {
+        (true, row_win)
+    } else {
+        (false, row_win)
+    }
 }
 
 //---------- Build vec of boards -------------------------------------
@@ -221,19 +178,35 @@ fn build_vec_of_boards(
     let split = all_boards.split_at(25);
     return_vec.push(split.0.to_vec());
     let remaining_boards = split.1.to_vec();
-    if remaining_boards.len() > 25 {
+    if remaining_boards.len() >= 25 {
         build_vec_of_boards(&remaining_boards, return_vec)
     } else {
+        //println!("return_vec !ABC::: {:?}", return_vec);
+
         return_vec
     }
 }
 
-//--- take all boards (flat) and initialise with (n, false) ---------------
+//1 97 1 1 1
+//1 7 1 1 1
+//1 75 1 1 1
+//1 66 1 1 1
+//1 8 1 1 67
 
-pub fn all_boards_tuple(boards: Vec<u8>) -> Vec<(u8, bool)> {
-    let mut checked_tuple_vec: Vec<(u8, bool)> = Vec::new();
-    boards
-        .iter()
-        .for_each(|n| checked_tuple_vec.push((*n, false)));
-    checked_tuple_vec
-}
+//97 7 75 66 8
+//1 1 1 1 1
+//1 1 1 1 1
+//1 1 1 1 1
+//1 1 1 1 67
+//
+//97 7 75 66 8
+//1 1 1 1 1
+//1 1 1 1 1
+//1 1 1 1 1
+//1 1 1 1 67
+//
+//2 1 1 1 5
+//4 5 5 5 4
+//4 5 5 5 4
+//4 5 5 5 4
+//5 1 1 1 67
